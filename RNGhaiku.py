69f4c9920abae7a745a5d0943480e1e24798ffa4
Joe <63716060+jsphwllng@users.noreply.github.com>
2020-09-15 15:46:56 +0200

    Removing API keys

--- RNGhaiku.py
+++ RNGhaiku.py
@@ -1,6 +1,7 @@
 import random
 import tweepy
 from words import words
+import creds
 
 def syllable_count(word):
 	word = word.lower()
@@ -42,10 +43,10 @@ def tweet_haiku():
 
 	print(completed_haiku)
 
-	consumer_key = "moSinewNqmGJ6Sq8oVUPCPcsa"
-	consumer_secret = "C6Dl8LF5L6ZCX4jqT4BrY1V7OlLrkeZ07U32ZceGlv02yVtOgt"
-	access_token = "1250184561050423302-lCSbevposK3tFo0SAU2jRkQZ3ROBa4"
-	access_token_secret = "q9vGQSDrykrabHOjhSj1AuXzx9qWqoeGbSloMZIdsdDxm"
+	consumer_key = creds.consumer_key
+	consumer_secret = creds.consumer_secret
+	access_token = creds.access_token
+	access_token_secret = creds.access_token_secret
 	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 	auth.set_access_token(access_token, access_token_secret)
 	api = tweepy.API(auth)
