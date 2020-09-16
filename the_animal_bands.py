6655aaf0f9314b4755ee39968ebfe40e8451f45a
joseph welling <joseph.welling@hotmail.com>
2020-09-16 10:38:18 +0200

    🎷🐛 updated emojis

--- the_animal_bands.py
+++ the_animal_bands.py
@@ -12,10 +12,10 @@ band_name_2 = ["Beasts", "Birds", "Walkers", "Roamers", "Surfers", "Boys", "Girl
 #odd
 
 def publictweet(tweet):
-	consumer_key = creds.consumer_key
-	consumer_secret = creds.consumer_secret
-	access_token = creds.access_token
-	access_token_secret = creds.access_token_secret
+	consumer_key = "cQkb9fyBoblzT5sFsin8rrJMp"
+	consumer_secret = "UJfaXMs960YNExD3yGpgwjjqAgvUoOi9pR9Iyhuie9AjGjLxSQ"
+	access_token = "1251862586120863746-FAGWtXltNWBY7JKQyznfVtOxjOwkXM"
+	access_token_secret = "4Sv4ZVgasOdktgQFgLa9UeKQ4btfPFhEbKmrshI9gmrdC"
 	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 	auth.set_access_token(access_token, access_token_secret)
 	api = tweepy.API(auth)
@@ -38,5 +38,3 @@ def talent_scout():
     the_boys = ''.join(bandassemble)
     tweet = the_boys + the_band_name
     publictweet(tweet)
-
-talent_scout()
